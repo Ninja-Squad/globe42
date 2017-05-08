613a2e00dd2778494fec0b556530f6a515bb49db
cexbrayat <cedric@ninja-squad.com>
2017-05-08 11:58:17 +0200

    feat: better user detail

--- frontend/src/app/user/user.component.spec.ts
+++ frontend/src/app/user/user.component.spec.ts
@@ -4,10 +4,17 @@ import { ActivatedRoute } from '@angular/router';
 
 import { UserComponent } from './user.component';
 import { AppModule } from '../app.module';
+import { UserModel } from '../models/user.model';
 
 describe('UserComponent', () => {
+  const user: UserModel = {
+    id: 0, firstName: 'John', lastName: 'Doe', surName: 'john', birthDate: '1980-01-01',
+    mediationCode: 'code1', address: 'Chemin de la gare', zipCode: 42000,
+    city: 'Saint Etienne', email: 'john@mail.com', isAdherent: true, entryDate: '2016-12-01'
+  };
+
   const activatedRoute = {
-    snapshot: { data: { user: { firstName: 'John', lastName: 'Doe' } } }
+    snapshot: { data: { user } }
   };
 
   beforeEach(() => TestBed.configureTestingModule({
@@ -20,7 +27,19 @@ describe('UserComponent', () => {
     fixture.detectChanges();
 
     const nativeElement = fixture.nativeElement;
-    const name = nativeElement.querySelector('p.form-control-static');
-    expect(name.textContent).toBe('John Doe');
+    const name = nativeElement.querySelector('#fullName');
+    expect(name.textContent).toBe('John Doe dit john');
+    const address = nativeElement.querySelector('#fullAddress');
+    expect(address.textContent).toBe('Chemin de la gare, 42000 Saint Etienne');
+    const birthDate = nativeElement.querySelector('#birthDate');
+    expect(birthDate.textContent).toBe('1980-01-01');
+    const email = nativeElement.querySelector('#email');
+    expect(email.textContent).toBe('john@mail.com');
+    const adherent = nativeElement.querySelector('#isAdherent');
+    expect(adherent.textContent).toBe('Oui');
+    const entryDate = nativeElement.querySelector('#entryDate');
+    expect(entryDate.textContent).toBe('2016-12-01');
+    const mediationCode = nativeElement.querySelector('#mediationCode');
+    expect(mediationCode.textContent).toBe('code1');
   });
 });
