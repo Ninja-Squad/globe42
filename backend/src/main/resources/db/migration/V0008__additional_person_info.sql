4b56394c5b12d71aa0bb350ca564291d28253034
jnizet <jb@ninja-squad.com>
2017-07-24 18:06:59 +0200

    fix(person): don't set new columns to not null before they've been given a value

--- backend/src/main/resources/db/migration/V0008__additional_person_info.sql
+++ backend/src/main/resources/db/migration/V0008__additional_person_info.sql
@@ -10,11 +10,11 @@ CREATE SEQUENCE family_situation_seq START WITH 1000;
 
 
 ALTER TABLE person
-  ADD COLUMN housing VARCHAR NOT NULL;
+  ADD COLUMN housing VARCHAR;
 ALTER TABLE person
   ADD COLUMN housing_space INT;
 ALTER TABLE person
-  ADD COLUMN fiscal_status VARCHAR NOT NULL;
+  ADD COLUMN fiscal_status VARCHAR;
 ALTER TABLE person
   ADD COLUMN fiscal_status_date DATE;
 ALTER TABLE person
@@ -29,8 +29,12 @@ ALTER TABLE person
 ALTER TABLE person
   ADD CONSTRAINT person_fk2 FOREIGN KEY (abroad_family_situation_id) REFERENCES family_situation (id);
 
-UPDATE person set fiscal_status_up_to_date = FALSE;
+UPDATE person set housing = 'UNKNOWN' WHERE housing IS NULL;
+UPDATE person set fiscal_status = 'UNKNOWN' WHERE fiscal_status IS NULL;
+UPDATE person set fiscal_status_up_to_date = FALSE where fiscal_status_up_to_date IS NULL;
 UPDATE person set marital_status = 'UNKNOWN' WHERE marital_status IS NULL;
 
-ALTER TABLE person ALTER marital_status SET NOT NULL;
+ALTER TABLE person ALTER housing SET NOT NULL;
+ALTER TABLE person ALTER fiscal_status SET NOT NULL;
 ALTER TABLE person ALTER fiscal_status_up_to_date SET NOT NULL;
+ALTER TABLE person ALTER marital_status SET NOT NULL;
