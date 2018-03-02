ff7cf32ed60d0ed0b7a1f77358e07947b52ff0d2
jnizet <jb@ninja-squad.com>
2018-03-02 12:38:15 +0100

    doc: fix typo in script doc

--- backend/scripts/importBackupLocally.sh
+++ backend/scripts/importBackupLocally.sh
@@ -5,7 +5,7 @@
 # The first step is to download a backup locally, from the Clever Cloud console - PostgreSQL addon
 # (https://console.clever-cloud.com/organisations/orga_dd753560-9dfe-4c93-a891-c639d138354b/addons/addon_9fbf3f9f-4f62-4c41-a086-37990d898f33)
 #
-# You can then clean your local database schema using ./gradlew/flywayCleanInteg, and then import the downloaded backup
+# You can then clean your local database schema using ./gradlew flywayCleanInteg, and then import the downloaded backup
 # by executing this script with the downloaded dump file path as argument.
 #
 # The script will ask for the local database password: globe42
