1f2be55c9d9447e58ebc11bb0bd585c93dfd8c39
jnizet <jb@ninja-squad.com>
2018-03-24 14:51:10 +0100

    chore: upgrade to angular-cli 6
    Notes:
    
     - I had to remove `--reporters dots` in package.json, because the option is not supported anymore. I replaced it by testing the CI env variable (set by Travis) in the karma.conf.js file
     - the ng update script sucks: I did everything by hand
     - I deleted the unique e2e spec, since we never run it, and it was wrong anyway

--- frontend/e2e/protractor.conf.js
+++ frontend/e2e/protractor.conf.js
@@ -6,7 +6,7 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
-    './e2e/**/*.e2e-spec.ts'
+    './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
     'browserName': 'chrome'
@@ -21,7 +21,7 @@ exports.config = {
   },
   beforeLaunch: function() {
     require('ts-node').register({
-      project: 'e2e/tsconfig.e2e.json'
+      project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
   },
   onPrepare() {
