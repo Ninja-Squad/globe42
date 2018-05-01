9b64453aafa2425a8eab1d446afc23aa1f3957cd
jnizet <jb@ninja-squad.com>
2018-05-01 08:31:53 +0200

    chore: format SQL script

--- backend/src/main/resources/db/migration/V0029__update_health_care_coverage_and_insurance.sql
+++ backend/src/main/resources/db/migration/V0029__update_health_care_coverage_and_insurance.sql
@@ -1,20 +1,26 @@
 UPDATE person
-SET health_care_coverage = 'PUMA' WHERE health_care_coverage = 'CMU';
+SET health_care_coverage = 'PUMA'
+WHERE health_care_coverage = 'CMU';
 
 UPDATE person
-SET health_care_coverage = 'AGR' WHERE health_care_coverage = 'MSA';
+SET health_care_coverage = 'AGR'
+WHERE health_care_coverage = 'MSA';
 
 UPDATE person
-SET health_care_coverage = 'SSI' WHERE health_care_coverage = 'RSI';
+SET health_care_coverage = 'SSI'
+WHERE health_care_coverage = 'RSI';
 
 UPDATE person
-SET health_care_coverage = 'OTHER' WHERE health_care_coverage = 'SPECIAL';
+SET health_care_coverage = 'OTHER'
+WHERE health_care_coverage = 'SPECIAL';
 
 UPDATE person
-SET health_insurance = 'UNKNOWN' WHERE health_insurance IS NULL OR health_insurance = '';
+SET health_insurance = 'UNKNOWN'
+WHERE health_insurance IS NULL OR health_insurance = '';
 
 UPDATE person
-SET health_insurance = 'MUTUELLE' WHERE health_insurance != 'UNKNOWN' AND health_insurance != 'CMUC';
+SET health_insurance = 'MUTUELLE'
+WHERE health_insurance != 'UNKNOWN' AND health_insurance != 'CMUC';
 
 ALTER TABLE person
   ALTER health_insurance SET NOT NULL;
