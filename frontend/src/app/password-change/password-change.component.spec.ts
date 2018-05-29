44233c0dd9b7d4884841fcb1a110278171a29a76
jnizet <jb@ninja-squad.com>
2018-05-29 07:55:01 +0200

    refactor: use ngx-speculoos on password change form

--- frontend/src/app/password-change/password-change.component.spec.ts
+++ frontend/src/app/password-change/password-change.component.spec.ts
@@ -7,74 +7,74 @@ import { CurrentUserModule } from '../current-user/current-user.module';
 import { CurrentUserService } from '../current-user/current-user.service';
 import { of, throwError } from 'rxjs';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ComponentTester, speculoosMatchers } from 'ngx-speculoos';
+
+class PasswordChangeComponentTester extends ComponentTester<PasswordChangeComponent> {
+
+  constructor() {
+    super(PasswordChangeComponent);
+  }
+
+  get title() {
+    return this.element('h1');
+  }
+
+  get oldPassword() {
+    return this.input('#oldPassword');
+  }
+
+  get newPassword() {
+    return this.input('#newPassword');
+  }
+
+  get submit() {
+    return this.button('#submit');
+  }
+}
 
 describe('PasswordChangeComponent', () => {
 
-  beforeEach(async(() => TestBed.configureTestingModule({
-    imports: [CurrentUserModule.forRoot(), HttpClientModule, ReactiveFormsModule, RouterTestingModule],
-    declarations: [PasswordChangeComponent],
-  })));
+  let tester: PasswordChangeComponentTester;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [CurrentUserModule.forRoot(), HttpClientModule, ReactiveFormsModule, RouterTestingModule],
+      declarations: [PasswordChangeComponent],
+    });
+
+    tester = new PasswordChangeComponentTester();
+    tester.detectChanges();
+
+    jasmine.addMatchers(speculoosMatchers);
+  }));
 
   it('should have a form', () => {
-    const fixture = TestBed.createComponent(PasswordChangeComponent);
-
-    // when we trigger the change detection
-    fixture.detectChanges();
-
-    // then we should have a field model
-    const componentInstance = fixture.componentInstance;
-    expect(componentInstance.passwordChangeForm.value).toEqual({oldPassword: '', newPassword: ''});
+    expect(tester.componentInstance.passwordChangeForm.value).toEqual({oldPassword: '', newPassword: ''});
   });
 
   it('should have a title', () => {
-    const fixture = TestBed.createComponent(PasswordChangeComponent);
-
-    // when we trigger the change detection
-    fixture.detectChanges();
-
-    // then we should have a title
-    const element = fixture.nativeElement;
-    expect(element.querySelector('h1').textContent).toContain('Changement de mot de passe');
+    expect(tester.title).toContainText('Changement de mot de passe');
   });
 
   it('should validate the form', () => {
-    const fixture = TestBed.createComponent(PasswordChangeComponent);
-    fixture.detectChanges();
+    tester.submit.click();
 
-    const element: HTMLElement = fixture.nativeElement;
-    const submitButton: HTMLButtonElement = element.querySelector('#submit');
-    submitButton.click();
-
-    expect(element.textContent).toContain('Le mot de passe actuel est obligatoire');
-    expect(element.textContent).toContain('Le nouveau mot de passe est obligatoire');
+    expect(tester.testElement).toContainText('Le mot de passe actuel est obligatoire');
+    expect(tester.testElement).toContainText('Le nouveau mot de passe est obligatoire');
   });
 
   it('should check the old password and change it via the user service and redirect if success', () => {
-    const fixture = TestBed.createComponent(PasswordChangeComponent);
-    fixture.detectChanges();
-
     const userService = TestBed.get(CurrentUserService);
     spyOn(userService, 'checkPassword').and.returnValue(of(null));
     spyOn(userService, 'changePassword').and.returnValue(of(null));
 
-    const element: HTMLElement = fixture.nativeElement;
-
-    const oldPassword: HTMLInputElement = element.querySelector('#oldPassword');
-    oldPassword.value = 'old';
-    oldPassword.dispatchEvent(new Event('input'));
-    fixture.detectChanges();
-
-    const newPassword: HTMLInputElement = element.querySelector('#newPassword');
-    newPassword.value = 'new';
-    newPassword.dispatchEvent(new Event('input'));
-    fixture.detectChanges();
-
     const router: Router = TestBed.get(Router);
     spyOn(router, 'navigate');
 
-    const submitButton: HTMLButtonElement = element.querySelector('#submit');
-    submitButton.click();
-    fixture.detectChanges();
+    tester.oldPassword.fillWith('old');
+    tester.newPassword.fillWith('new');
+
+    tester.submit.click();
 
     expect(userService.checkPassword).toHaveBeenCalledWith('old');
     expect(userService.changePassword).toHaveBeenCalledWith('new');
@@ -84,68 +84,40 @@ describe('PasswordChangeComponent', () => {
   });
 
   it('should call the user service and display a message if check password fails', () => {
-    const fixture = TestBed.createComponent(PasswordChangeComponent);
-    fixture.detectChanges();
-
     const userService = TestBed.get(CurrentUserService);
     spyOn(userService, 'checkPassword').and.returnValue(throwError(null));
     spyOn(userService, 'changePassword');
 
-    const element: HTMLElement = fixture.nativeElement;
-
-    const oldPassword: HTMLInputElement = element.querySelector('#oldPassword');
-    oldPassword.value = 'old';
-    oldPassword.dispatchEvent(new Event('input'));
-    fixture.detectChanges();
-
-    const newPassword: HTMLInputElement = element.querySelector('#newPassword');
-    newPassword.value = 'new';
-    newPassword.dispatchEvent(new Event('input'));
-    fixture.detectChanges();
-
     const router: Router = TestBed.get(Router);
     spyOn(router, 'navigate');
 
-    const submitButton: HTMLButtonElement = element.querySelector('#submit');
-    submitButton.click();
-    fixture.detectChanges();
+    tester.oldPassword.fillWith('old');
+    tester.newPassword.fillWith('new');
+
+    tester.submit.click();
 
     expect(userService.checkPassword).toHaveBeenCalledWith('old');
     expect(userService.changePassword).not.toHaveBeenCalled();
     expect(router.navigate).not.toHaveBeenCalled();
-    expect(element.textContent).toContain('Le mot de passe n\'a pas été modifié.');
+    expect(tester.testElement).toContainText('Le mot de passe n\'a pas été modifié.');
   });
 
   it('should call the user service and display a message if change password fails', () => {
-    const fixture = TestBed.createComponent(PasswordChangeComponent);
-    fixture.detectChanges();
-
     const userService = TestBed.get(CurrentUserService);
     spyOn(userService, 'checkPassword').and.returnValue(of(null));
     spyOn(userService, 'changePassword').and.returnValue(throwError(null));
 
-    const element: HTMLElement = fixture.nativeElement;
-
-    const oldPassword: HTMLInputElement = element.querySelector('#oldPassword');
-    oldPassword.value = 'old';
-    oldPassword.dispatchEvent(new Event('input'));
-    fixture.detectChanges();
-
-    const newPassword: HTMLInputElement = element.querySelector('#newPassword');
-    newPassword.value = 'new';
-    newPassword.dispatchEvent(new Event('input'));
-    fixture.detectChanges();
-
     const router: Router = TestBed.get(Router);
     spyOn(router, 'navigate');
 
-    const submitButton: HTMLButtonElement = element.querySelector('#submit');
-    submitButton.click();
-    fixture.detectChanges();
+    tester.oldPassword.fillWith('old');
+    tester.newPassword.fillWith('new');
+
+    tester.submit.click();
 
     expect(userService.checkPassword).toHaveBeenCalledWith('old');
     expect(userService.changePassword).toHaveBeenCalledWith('new');
     expect(router.navigate).not.toHaveBeenCalled();
-    expect(element.textContent).toContain('Le mot de passe n\'a pas été modifié.');
+    expect(tester.testElement).toContainText('Le mot de passe n\'a pas été modifié.');
   });
 });
