64e557309b1235daf2c70f6e908b485678a0a776
jnizet <jb@ninja-squad.com>
2018-12-25 16:25:07 +0100

    refactor: remove ugly shifting in navigation progress

--- frontend/src/app/navigation-progress/navigation-progress.component.ts
+++ frontend/src/app/navigation-progress/navigation-progress.component.ts
@@ -3,6 +3,10 @@ import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Rout
 import { merge, Observable, timer } from 'rxjs';
 import { filter, first, map, switchMap, takeUntil, takeWhile } from 'rxjs/operators';
 
+export interface Progress {
+  value: number;
+}
+
 @Component({
   selector: 'gl-navigation-progress',
   templateUrl: './navigation-progress.component.html',
@@ -11,7 +15,7 @@ import { filter, first, map, switchMap, takeUntil, takeWhile } from 'rxjs/operat
 })
 export class NavigationProgressComponent implements OnInit {
 
-  value$: Observable<number>;
+  progress$: Observable<Progress | null>;
 
   constructor(private router: Router) { }
 
@@ -20,19 +24,19 @@ export class NavigationProgressComponent implements OnInit {
       filter(e => e instanceof NavigationEnd || e instanceof NavigationCancel || e instanceof NavigationError)
     );
 
-    this.value$ = this.router.events.pipe(
+    this.progress$ = this.router.events.pipe(
       filter(e => e instanceof NavigationStart), // trigger the emission when a navigation starts
       switchMap(() => merge( // then emit every 500ms, and when the navigation ends or cancels or errors. This delay must
                              // match the transition duration in the .scss file
         timer(0, 500).pipe(
           map(i => 100 - (100 / Math.pow(2, i))), // emit 0 then 50, 75, 87.5, etc.
           takeWhile(i => i < 99.95), // stop because that just triggers change detection with no visual change anymore
-          takeUntil(end$) // but stop emitting every 500ms when the navigation ends or cancels or errors
+          takeUntil(end$), // but stop emitting every 500ms when the navigation ends or cancels or errors
+          map(i => ({ value: i }))
         ),
-        end$.pipe(first(), map(() => -1)) // set back to -1 when the navigation ends or cancels or errors
-                                          // because the value is shifted (see below)
-      )),
-      map(i => i + 1)  // shift 1 rather than 0 and be able to use *ngIf="value$ | async as value". This sucks.
+        end$.pipe(first(), map(() => null)) // set back to null when the navigation ends or cancels or errors to hide
+                                            // the progress bar
+      ))
     );
   }
 }
