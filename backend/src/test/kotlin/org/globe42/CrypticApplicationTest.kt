6dad7d29e2ecc9052b759b37a522b694009e0f8c
jnizet <jb@ninja-squad.com>
2019-03-09 16:34:00 +0100

    refactor: improve tests
    - avoid using unnecessary ExtendWith annotation (since XxxTest is already annotated with it)
    - make AdminOnlyIntegrationTest faster by only loading a tiny context instead of the whole context

--- backend/src/test/kotlin/org/globe42/CrypticApplicationTest.kt
+++ backend/src/test/kotlin/org/globe42/CrypticApplicationTest.kt
@@ -1,12 +1,9 @@
 package org.globe42
 
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.TestPropertySource
-import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@ExtendWith(SpringExtension::class)
 @SpringBootTest
 @TestPropertySource("/test.properties")
 class CrypticApplicationTest {
