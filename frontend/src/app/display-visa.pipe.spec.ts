d6715e72a390695705bed4ba915e0fb695946a6b
Jean-Baptiste Nizet <jb@ninja-squad.com>
2019-07-26 08:58:28 +0200

    feat: additional visa type

---
+++ frontend/src/app/display-visa.pipe.spec.ts
@@ -0,0 +1,9 @@
+import { DisplayVisaPipe } from './display-visa.pipe';
+
+describe('DisplayVisaPipe', () => {
+  it('should visa', () => {
+    const pipe = new DisplayVisaPipe();
+    expect(pipe.transform('NONE')).toBe('Aucun');
+    expect(pipe.transform('SHORT_STAY')).toBe('C (court séjour)');
+  });
+});
