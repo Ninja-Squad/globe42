33fefab079531cb18cd39c9c9ea18978e3077c34
Jean-Baptiste Nizet <jb@ninja-squad.com>
2020-03-03 23:04:30 +0100

    fix: add a filter to (hopefully) fix the links generation in the actuator

---
+++ backend/src/main/kotlin/org/globe42/web/HttpsActuatorConfig.kt
@@ -0,0 +1,22 @@
+package org.globe42.web
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
+import org.springframework.web.filter.ForwardedHeaderFilter
+
+/**
+ * Configuration class used to add a servlet filter used to make the actuator think that it's
+ * running on HTTPS, and thus generate the correct URLs, in production where the app sits behind a proxy
+ * @author JB Nizet
+ */
+@Configuration
+class HttpsActuatorConfig {
+    @Bean
+    fun forwardedHeaderFilterFilterRegistrationBean(): FilterRegistrationBean<ForwardedHeaderFilter>? {
+        return FilterRegistrationBean(ForwardedHeaderFilter()).apply {
+            setOrder(Ordered.HIGHEST_PRECEDENCE)
+        }
+    }
+}
