9b1fb20a5a39fbc726503f10ad40423651029f80
Jean-Baptiste Nizet <jb@ninja-squad.com>
2020-05-30 17:18:31 +0200

    feat: use h2 instead of h5

--- frontend/src/app/persons-reports/persons-reports.component.ts
+++ frontend/src/app/persons-reports/persons-reports.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'gl-person-reports',
   templateUrl: './persons-reports.component.html',
-  styleUrls: ['./persons-reports.component.css']
+  styleUrls: ['./persons-reports.component.scss']
 })
 export class PersonsReportsComponent {}
