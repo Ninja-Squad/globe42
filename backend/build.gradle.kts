77379ce9bec6c4505173c8eb71028871d0880a47
Jean-Baptiste Nizet <jb@ninja-squad.com>
2020-08-13 22:39:42 +0200

    chore: upgrade gradle to 6.2, boot to 2.3.3 and SpringMockK to 2.0.3

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
     id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.noarg") version kotlinVersion
-    id("org.springframework.boot") version "2.3.2.RELEASE"
+    id("org.springframework.boot") version "2.3.3.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     id("org.flywaydb.flyway") version "6.4.4"
     id("com.gorylenko.gradle-git-properties") version "2.2.2"
@@ -133,7 +133,7 @@ dependencies {
         exclude(module = "junit")
         exclude(module = "mockito")
     }
-    testImplementation("com.ninja-squad:springmockk:2.0.2")
+    testImplementation("com.ninja-squad:springmockk:2.0.3")
     testImplementation("io.mockk:mockk:1.10.0")
 
     testImplementation("com.ninja-squad:DbSetup:2.1.0")
