7290f920d23b72732577440368254ef862a9b886
jnizet <jb@ninja-squad.com>
2020-11-12 08:54:01 +0100

    chore: upgrade to Angular and Angular CLI 11

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -9,16 +9,20 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, './coverage/globe42'),
-      reports: ['html', 'lcovonly', 'json', 'text-summary'],
-      fixWebpackSourcePaths: true
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'lcovonly' }
+      ],
     },
     reporters: process.env.CI === 'true' ? ['dots'] : ['progress', 'kjhtml'],
     port: 9876,
