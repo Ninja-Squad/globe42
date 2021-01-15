a9a11c114825c236d6940d9b1fccb7ae4f5fb0a1
jnizet <jb@ninja-squad.com>
2021-01-15 08:49:02 +0100

    chore: upgrade to Spring Boot 2.4.2

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
     id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.noarg") version kotlinVersion
-    id("org.springframework.boot") version "2.4.1"
+    id("org.springframework.boot") version "2.4.2"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
     id("org.flywaydb.flyway") version "7.1.1"
     id("com.gorylenko.gradle-git-properties") version "2.2.2"
