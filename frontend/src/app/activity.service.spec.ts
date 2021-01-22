cfd42147132626e6fd2643442131e09f2f6b7cd1
jnizet <jb@ninja-squad.com>
2021-01-22 19:00:49 +0100

    test: add missing tests

--- frontend/src/app/activity.service.spec.ts
+++ frontend/src/app/activity.service.spec.ts
@@ -4,7 +4,7 @@ import { ActivityService } from './activity.service';
 import { HttpTester } from './http-tester.spec';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { Page } from './models/page';
-import { Activity, ActivityCommand, ActivityModel } from './models/activity.model';
+import { Activity, ActivityCommand, ActivityModel, ActivityReport } from './models/activity.model';
 import { activityType } from './models/activity-type.model';
 
 describe('ActivityService', () => {
@@ -67,6 +67,35 @@ describe('ActivityService', () => {
     } as Page<Activity>);
   });
 
+  it('should get an activity', () => {
+    const backendActivity: ActivityModel = {
+      id: 21,
+      date: '2021-01-01',
+      type: 'MEAL',
+      participants: [
+        {
+          id: 42
+        }
+      ]
+    } as ActivityModel;
+
+    let actualActivity: Activity;
+    service.get(21).subscribe(a => (actualActivity = a));
+
+    http.expectOne({ method: 'GET', url: '/api/activities/21' }).flush(backendActivity);
+
+    expect(actualActivity).toEqual({
+      id: 21,
+      date: '2021-01-01',
+      type: activityType('MEAL'),
+      participants: [
+        {
+          id: 42
+        }
+      ]
+    } as Activity);
+  });
+
   it('should delete an activity', () => {
     httpTester.testDelete('/api/activities/42', service.delete(42));
   });
@@ -105,5 +134,57 @@ describe('ActivityService', () => {
   it('should update an activity', () => {
     const command = { type: 'MEAL' } as ActivityCommand;
     httpTester.testPut('/api/activities/42', command, service.update(42, command));
+  });
+
+  it('should get an activity report', () => {
+    const backendReport: ActivityReport = {
+      totalActivityCount: 10,
+      presences: [
+        {
+          activityCount: 5,
+          person: {
+            firstName: 'JB',
+            lastName: 'Nizet'
+          }
+        },
+        {
+          activityCount: 7,
+          person: {
+            firstName: 'Claire',
+            lastName: 'Brucy'
+          }
+        }
+      ]
+    } as ActivityReport;
+
+    let actualReport: ActivityReport;
+    service.report('MEAL', '2021-01-01', '2021-01-31').subscribe(r => (actualReport = r));
+
+    http
+      .expectOne({
+        method: 'GET',
+        url: '/api/activities/reports?type=MEAL&from=2021-01-01&to=2021-01-31'
+      })
+      .flush(backendReport);
+
+    expect(actualReport).toEqual({
+      totalActivityCount: 10,
+      presences: [
+        {
+          activityCount: 7,
+          person: {
+            firstName: 'Claire',
+            lastName: 'Brucy'
+          }
+        },
+        {
+          activityCount: 5,
+          person: {
+            firstName: 'JB',
+            lastName: 'Nizet'
+          }
+        }
+      ]
+    } as ActivityReport);
   });
 });
