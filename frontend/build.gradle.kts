3b064458b602d2adb2b36f88455acfd274d329ea
jnizet <jb@ninja-squad.com>
2021-04-11 17:48:56 +0200

    build: simplify lint task by using max-warnings option

--- frontend/build.gradle.kts
+++ frontend/build.gradle.kts
@@ -56,13 +56,6 @@ tasks {
 
     val lint by registering {
         dependsOn(yarnLint)
-        doLast {
-            file("$buildDir/eslint-result.txt").useLines { sequence ->
-                if (sequence.any { it.contains("warning") }) {
-                    throw GradleException("Lint warning found. Check eslint-result.txt")
-                }
-            }
-        }
     }
 
     val yarnCodecov by registering(YarnTask::class) {
